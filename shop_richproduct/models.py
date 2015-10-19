247e14e686f0b5e1291f5eb195c54b18e44e0e06
Eric Amador <eric.amador14@gmail.com>
2015-10-19 10:32:54 -0400

    Adding a purchase_button_label field to the RichProductMixin.
    This new field allows you to customize the purchase button text on a
    product-by-product basis. A new migration has also been created for this
    change.

--- shop_richproduct/models.py
+++ shop_richproduct/models.py
@@ -11,6 +11,11 @@ class RichProductMixin(models.Model):
     description = models.CharField(max_length=255)
     description_placeholder = PlaceholderField('description_placeholder')
     image = models.ImageField(upload_to="rich_product")
+    purchase_button_label = models.CharField(
+        default="Purchase",
+        max_length=64,
+        help_text="The text that appears on this "
+                  "product's purchase button")
 
     class Meta(object):
         abstract = True
